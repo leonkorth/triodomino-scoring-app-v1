aa0ab6ce8b2d572e5205ccf729d70b4a97d938c0
leonkorth <leonkorth@web.de>
2022-02-15 15:34:47 +0100

    first

---
+++ src/main/java/leonkorth/tridominoscoringapp/TridominoScoringAppApplication.java
@@ -0,0 +1,13 @@
+package leonkorth.tridominoscoringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TridominoScoringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TridominoScoringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/leonkorth/tridominoscoringapp/TridominoScoringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package leonkorth.tridominoscoringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TridominoScoringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
